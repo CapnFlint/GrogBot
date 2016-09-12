f9a3ab0c1753ea81253995f6387f9455ee588141
Dave Belcher <ukbelch@gmail.com>
2016-09-12 01:51:59 -0700

    Fixes to teespring monitor
    Fixed a namespace collision issue

--- core/monitors/teespring.py
+++ core/monitors/teespring.py
@@ -3,7 +3,7 @@ import random
 import time
 import thread
 
-import utils.teespring as teespring
+import utils.teespring as tee_utils
 
 import modules.overlay.overlay as overlay
 
@@ -11,14 +11,14 @@ class teespring():
     def __init__(self, owner, query, international = False):
         self.query = query
         self.intl = international
-        self.count = teespring.get_orders(self.query, self.intl)
+        self.count = tee_utils.get_orders(self.query, self.intl)
         logging.info("Teespring monitor initialized")
 
     def teespring_thread(self):
         cooldown = 35
         while 1:
             time.sleep(cooldown)
-            count = teespring.get_orders(self.query, self.intl)
+            count = tee_utils.get_orders(self.query, self.intl)
             if count > self.count:
                 self.count += 1
                 overlay.alert_teespring(self.count)
