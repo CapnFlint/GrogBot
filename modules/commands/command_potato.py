6a8c2c0cc654daed4e39d2230b00c63c3c496b9f
Dave Belcher <ukbelch@gmail.com>
2017-03-02 19:27:59 -0800

    made !potato an official command, so it sends emotes to the raid defence

---
+++ modules/commands/command_potato.py
@@ -0,0 +1,10 @@
+from helper import *
+import utils.twitch_utils as utils
+
+import modules.overlay.overlay as overlay
+
+# retrieves a quote
+@processes("!potato")
+def command_quote(self, sender, args):
+    self.connMgr.send_message("capnYarr capnHype capnYarr capnHype capnYarr capnHype capnYarr capnHype capnYarr capnHype capnYarr capnHype")
+    overlay.send_emotes(['138934','138934','138934','138934','138934','138934','81534','81534','81534','81534','81534','81534','81534'])
