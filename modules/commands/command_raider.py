1ecf0cdbbaf1b9ff6aa76684da87c73aa7a0c674
Dave Belcher <ukbelch@gmail.com>
2017-03-02 20:20:48 -0800

    tweak to catch non-streamer plebs

--- modules/commands/command_raider.py
+++ modules/commands/command_raider.py
@@ -8,7 +8,7 @@ import modules.overlay.overlay as overlay
 def command_quote(self, sender, args):
     raider = args[0]
     streamer = twitch.check_streamer(raider)
-    if streamer:
+    if streamer and streamer['game']:
         self.connMgr.send_message("We are being raided by " + raider + "! DEFEND THE SHIP!!!")
         overlay.start_raid(raider, streamer['logo'])
     else:
