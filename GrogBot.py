114d89e112ee4c7004f8d7b4fc441009ea737247
Dave Belcher <ukbelch@gmail.com>
2017-04-25 22:07:37 -0700

    adding infinite loop while websocket does it's thing

--- GrogBot.py
+++ GrogBot.py
@@ -60,6 +60,8 @@ class GrogBot():
         # Connect and start bot
         logging.info("Starting Up!!!")
         #self.connMgr.connect()
+        while(1):
+            pass
 
 def main():
     grog = GrogBot(twitch.twitch_channel)
