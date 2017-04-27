942a7e386363f7465813589c04be8f5d35158b60
Dave Belcher <ukbelch@gmail.com>
2017-04-26 20:30:45 -0700

    added tests for the new sub tiers

--- modules/commands/command_test.py
+++ modules/commands/command_test.py
@@ -2,6 +2,14 @@ from helper import *
 import modules.overlay.overlay as overlay
 import utils.twitter as twitter
 
-@processes("!test", PERM_ADMIN)
-def command_test(self, sender, args):
-    overlay.alert_follow("GrogBot", True)
+@processes("!test1", PERM_ADMIN)
+def command_test1(self, sender, args):
+    overlay.alert_sub("GrogBot", "1", "1", "sub", "This is a basic sub!")
+
+@processes("!test2", PERM_ADMIN)
+def command_test2(self, sender, args):
+    overlay.alert_sub("GrogBot", "2", "1", "sub", "This is a $9.99 sub!")
+
+@processes("!test3", PERM_ADMIN)
+def command_test2(self, sender, args):
+    overlay.alert_sub("GrogBot", "3", "1", "sub", "This is a $24.99 sub!")
