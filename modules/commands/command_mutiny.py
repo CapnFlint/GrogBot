53caf9c5d78d525b44d10113b7987b79b824637e
Dave Belcher <ukbelch@gmail.com>
2017-06-07 00:02:32 -0700

    Check to see if the target is already dead + new mutiny command!

--- modules/commands/command_mutiny.py
+++ modules/commands/command_mutiny.py
@@ -1,6 +1,91 @@
+import time
+import thread
+import random
+
 from helper import *
 
-@processes("!mutiny")
+@processes("!mutiny", PERM_MOD)
 def command_mutiny(self, sender, args):
-    self.connMgr.send_message("The Capn catches " + sender + " trying to start a mutiny!")
-    self.run_command("!plank", [sender, 15])
+    self.mutineers = {}
+    self.defenders = {}
+    #self.connMgr.send_message("The Capn catches " + sender + " trying to start a mutiny!")
+    #self.run_command("!plank", [sender, 15])
+
+    # Have a mutiny window... Random event every X to Y minutes.
+    # a viewer starts a mutiny... anyone or subscriber?
+
+    # a timer starts, and others can choose to !join the mutiny, or !defend the captain
+    def mutiny_thread(self):
+        countdown = 120
+
+        def command_join(self, sender, args):
+            if check_permission(sender, PERM_FOLLOWS):
+                mutiny_join(self, sender, args)
+        self.add_command("!join", command_join)
+
+        def command_defend(self, sender, args):
+            if check_permission(sender, PERM_FOLLOWS):
+                mutiny_defend(self, sender, args)
+        self.add_command("!defend", command_defend)
+
+        time.sleep(countdown)
+
+        self.remove_command("!join")
+        self.remove_command("!defend")
+
+        if self.mutineers:
+            mutiny_start_mutiny(self)
+
+    # if mutiny isn't already running, and no other event is running...
+    thread.start_new_thread(mutiny_thread, (self,))
+    self.connMgr.send_message(Sender + " has started a mutiny! Would you like to !join the mutiny, or !defend the Captain?!")
+
+
+def mutiny_join(self, sender, args):
+    # handle adding player to the mutiny
+    if sender not in self.mutineers.keys() + self.defenders.keys():
+        self.mutineers[sender] = self.charMgr.load_character(sender)['level']
+
+def mutiny_defend(self, sender, args):
+    # handle adding a player to the defence
+    if sender not in self.mutineers + self.defenders:
+        self.defenders[sender] = self.charMgr.load_character(sender)['level']
+
+def sum_array(values):
+    total = 0
+    for i in values:
+        total += i
+
+def mutiny_start(self):
+    # See if mutiny succeeds (determine what this means)
+    result = 50 + sum_array(self.defenders.values()) - sum_array(self.mutineers.values())
+    print self.defenders
+    print self.mutineers
+    print result
+    if result <= 0:
+        self.connMgr.send_message("The Mutiny succeeds! Mutineers: " + ", ".join(self.mutineers))
+        # Success!
+        # If mutiny succeeds, start a mutiny timer. During timer, mutineers gain access
+        # to special functionality. What functionality?
+        # - Turn on song requests?
+        # - Manage song requests
+        # - Gain access to on-screen sounds/animations?
+        # - Can /plank anyone, including a mod? One /plank per mutineer.
+        # - Free grog for mutineers?
+        # - New captain mod for x minutes?
+        # - Can start a battle arena?
+        # - Start certain events (treasure hunt?)
+        # Vote for a new captain? Or person who started mutiny?
+    else:
+        self.connMgr.send_message("The Mutiny fails. Off the ship with ya!")
+        # Mutiny fail
+        # Else it fails, all mutineers get planked/timed out for 5 minutes. Lose EXP and Booty.
+        # Everyone who defends the ship gains exp and booty
+
+    # Timer ends, Grog regains control of the ship, returns it to the captain, mutineers
+    # get planked/timed out for X minutes, lose EXP and Booty.
+
+    self.defenders = {}
+    self.mutineers = {}
+
+    # cooldown timer
--- modules/commands/command_plank.py
+++ modules/commands/command_plank.py
@@ -16,9 +16,12 @@ def command_plank(self, sender, args):
 
     if self.charMgr.char_exists(target):
         if db.get_access(target) == 0:
-            self.connMgr.send_message(target + ', you have been sentenced to walk the PLANK!!!')
-            sound = [{"file": "sounds/davey.mp3", "volume": 50}, {"file": "sounds/splash.mp3", "volume": 60}]
-            overlay.alert(1, "[HL]{0}[/HL] has walked the PLANK!!!".format(target), sound)
-            self.charMgr.kill_character(target, duration)
+            if self.charMgr.is_alive(target):
+                self.connMgr.send_message(target + ', you have been sentenced to walk the PLANK!!!')
+                sound = [{"file": "sounds/davey.mp3", "volume": 50}, {"file": "sounds/splash.mp3", "volume": 60}]
+                overlay.alert(1, "[HL]{0}[/HL] has walked the PLANK!!!".format(target), sound)
+                self.charMgr.kill_character(target, duration)
+            else:
+                self.connMgr.send_message("Sorry sir, " + target + " is already dead!")
     else:
         self.connMgr.send_message(target + " doesn't exist!")
