93a404266f2ef34611b4ee81ee1b224f5484a48e
Capn Flint <capnflinttv@gmail.com>
2018-06-18 19:49:56 -0700

    deboog!

--- utils/twitch_utils.py
+++ utils/twitch_utils.py
@@ -7,6 +7,7 @@ from config.config import config
 
 def get_ids(names):
     url = "https://api.twitch.tv/kraken/users?login=" + ','.join(names)
+    logging.debug(url)
     try:
         req = urllib2.Request(url)
         req.add_header('Accept', 'application/vnd.twitchtv.v5+json')
