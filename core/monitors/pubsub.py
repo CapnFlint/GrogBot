c4860f83362c9410dd83597d60ae0da9b16f98a7
Capn Flint <capnflinttv@gmail.com>
2018-08-26 14:09:45 -0700

    pubsub fix... hopefully

--- core/monitors/pubsub.py
+++ core/monitors/pubsub.py
@@ -204,13 +204,13 @@ class pubsub():
 
     def _connect(self):
         print "Connecting..."
-        websocket.enableTrace(True)
+        #websocket.enableTrace(True)
         self.ws = websocket.WebSocketApp("wss://pubsub-edge.twitch.tv",
                                     on_message = self.on_message,
                                     on_error = self.on_error,
                                     on_close = self.on_close)
         self.ws.on_open = self.on_open
-        self.ws.run_forever()
+        self.ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 
     def start(self):
         thread.start_new_thread(self._connect, ())
