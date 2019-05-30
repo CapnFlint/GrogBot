030931a69624a2abb4418fbb61e927574b0c5c47
Capn Flint <capnflinttv@gmail.com>
2019-05-29 20:27:35 -0700

    wrong python version syntax :(

--- utils/twitch_utils.py
+++ utils/twitch_utils.py
@@ -7,7 +7,7 @@ from config.config import config
 
 def get_ids(names):
     # names is a list of names ['foo','bar','baz']
-    url = "https://api.twitch.tv/kraken/users?login=" + urllib.parse.quote(','.join(names))
+    url = "https://api.twitch.tv/kraken/users?login=" + urllib.quote(','.join(names))
     logging.debug("URL: " + url)
     try:
         req = urllib2.Request(url)
