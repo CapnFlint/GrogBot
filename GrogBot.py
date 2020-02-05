0624185ad2a043d7b1b09a143989a2f116449467
Capn Flint <capnflinttv@gmail.com>
2020-02-04 19:21:10 -0800

    Update GrogBot.py

--- GrogBot.py
+++ GrogBot.py
@@ -48,7 +48,7 @@ class GrogBot():
         # Initialize all the worker threads
         self.add_worker(random_messages(self))
         self.add_worker(follows(self))
-        self.add_worker(pubsub(self))
+        #self.add_worker(pubsub(self))
         #self.add_worker(bits(self))
         #self.add_worker(teespring(self, "spud citizen Capn_Flint", True))
         #self.add_worker(workers.twitter(self))
